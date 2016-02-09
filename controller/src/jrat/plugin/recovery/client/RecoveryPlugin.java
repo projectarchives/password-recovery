28c2a98132cce888c6d01b84fc56ac867a9ba898
redpois0n <red@cmail.nu>
2016-02-09 10:37:28 +0100

    Fix icon

--- controller/src/jrat/plugin/recovery/client/RecoveryPlugin.java
+++ controller/src/jrat/plugin/recovery/client/RecoveryPlugin.java
@@ -5,13 +5,21 @@ import jrat.api.Plugin;
 import jrat.api.net.Packet;
 import jrat.api.ui.RATMenuItem;
 
+import javax.swing.ImageIcon;
+
 public class RecoveryPlugin extends Plugin {
 	
 	public static final short HEADER = 130;
 	
 	public RecoveryPlugin() {
-		super("Password Recovery", "1.0", "Recover Passwords", "jRAT", Icons.getIcon("Password Recovery", "/icons/icon.png"));
-	
+		super("Password Recovery", "1.0", "Recover Passwords", "jRAT");
+
+		try {
+			icon = new ImageIcon(getResource("icons/icon.png"));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
 		RATMenuItem entry = new RATMenuItem(new MenuListener(), "Password Recovery", icon);
 		RATMenuItem.addItem(entry);
 		
